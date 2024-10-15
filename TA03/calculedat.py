b88499f3feb73cae4ecb18f866e0ab45ea1af907
didac.escoda.7e6 <didac.escoda.7e6@itb.cat>
2024-10-15 08:57:39 +0200

    tasca 3

--- TA03/calculedat.py
+++ TA03/calculedat.py
@@ -1,18 +1,26 @@
 
 from datetime import datetime
+try:
+    dia = int(input("introdueix el dia de neixament"))
+    mes = int(input("introdueix el mes de neixament"))
+    any = int(input("introdueix el any de neixament"))
+    any_actual = datetime.now().year
 
-dia = int(intput("introdueix el dia de neixament"))
 
-mes = int(intput("introdueix el mes de neixament"))
+    if dia > 31:
+        print("valor incorrecte")
 
-any = int(intput("introdueix el any de neixament"))
 
-if dia > 31:
-    print("valor incorrecte")
 
-elif mes > 12:
-    print("valor incorrecte")
-
-elif any > 2024:
-    print("valor incorrecte")
-
+    if any <= any_actual:
+        if mes >=1 and mes <=12:
+            if dia >=1 and dia <=31:
+                    edat = any_actual-any
+                    if edat >= 16 and edat <=65:
+                        print ("pots treballar")
+                    else:
+                        print("no tens edat per treballar")
+    else:
+        print("any no valid")
+except ValueError:
+    print("malament")
